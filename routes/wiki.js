e1e4f79a38732723e1988b341fbca76fec77476e
Mariya Bogorodova <mb2283@cornell.edu>
2016-01-27 16:17:36 -0500

    adds routes - post creates new db entry and displays page

--- routes/wiki.js
+++ routes/wiki.js
@@ -15,12 +15,54 @@ router.get('/', function(req, res, next){
 });
 
 router.post('/wiki/', function(req, res, next){
-	res.json(req.body);
+	
+	var statusV, preurlTitle;
+	if (req.body.open == 'on'){
+		statusV = "open"
+	} else {
+		statusV = 'closed'
+	}
+
+	//create a new document, an instance of pageModel
+	var page = new pageModel({
+		title: req.body.title,
+		content: req.body.page_content,
+		status: statusV,
+		//author: 
+	});
+
+	//before save(), get url from page title, assign random string to page title & url if necessary
+	page.pre('validate', function(next){
+		page.urlTitle = makeURLTitle(req.body.title);
+		if(req.body.title.length == 0) page.title = 'Random Title ' + page.urlTitle;
+		next();
+	});
+
+	page.save()
+	.then(function(){
+		res.redirect('/wiki/' + page.urlTitle);
+	})
+	.then(null, function(err){console.log(err)});
 });
 
 router.get('/wiki/add', function(req, res, next){
 	res.render('addpage');
 });
 
+router.get('/wiki/:urlTitle', function(req, res, next){
+	pageModel.findOne({urlTitle: req.params.urlTitle}).exec()
+	.then(function(page){
+		console.log(page);
+		res.render('wikipage', page);
+	})
+	.then(null, function(err){console.log(err)});
+});
+
+function makeURLTitle (title) {
+	if(title.length>0){
+		return title.replace(/\s+/g, '_').replace(/\W+/g, '');	
+	}
+	return Math.random().toString(36).substring(2,7);
+}
 // json post request:
 // {"author_name":"Emma","author_email":"me@gh.com","title":"Test","page_content":"Contents!","open":"","closed":""}
